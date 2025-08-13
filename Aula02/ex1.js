02593ef96b63249961f05acbb59c61c4e195fd7c
Miguel Santos <miguelsantos51346@gmail.com>
2025-08-13 20:48:48 -0300

    Aula02
    Concatenação de variáveis

--- Aula02/ex1.js
+++ Aula02/ex1.js
@@ -1,7 +1,12 @@
 // Exemplo 1
 const idade = 29;
 const nome = "Teste";
-
+const inf = nome + " " + idade;
 // Imprime no terminal
 
 console.log("Hello World");
+
+// Exibindo os valores das constantes no terminal
+
+console.log(`idade ${idade}`);
+console.log(inf);
