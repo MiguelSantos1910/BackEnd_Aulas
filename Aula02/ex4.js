5fb2c726b8809de4f2f91a2c7d62c3a6e7bc8207
Miguel Santos <miguelsantos51346@gmail.com>
2025-08-13 21:51:08 -0300

    Exemplo 4
    Declarando variável com var

---
+++ Aula02/ex4.js
@@ -0,0 +1,7 @@
+// Declarando variável com var
+
+if(1 > 0){
+    var valor = 20; //Declara variável com var, permite acessar a variável fora de escopo.
+    console.log(valor);
+}
+console.log(valor);
