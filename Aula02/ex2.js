9a18fd8025ac4513239194b95ca71936eff7ac3b
Miguel Santos <miguelsantos51346@gmail.com>
2025-08-13 21:34:51 -0300

    Exemplo 2
    Tipos de variáveis

---
+++ Aula02/ex2.js
@@ -0,0 +1,8 @@
+// Exemplo 2 verificando o tipo de variável
+
+const valor = 50.5;
+const num = 100;
+const a = true;
+console.log(valor, `Tipo de constante ${typeof valor}`);
+console.log(num, `Tipo de constante ${typeof num}`);
+console.log(a, `Tipo de constante ${typeof a}`);
